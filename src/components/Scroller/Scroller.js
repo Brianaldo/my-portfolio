483531fe14b005121f7fbd29982d081cbc45be6e
Brianaldo <brianaldophandiarta31@gmail.com>
2022-03-15 13:03:42 +0700

    add landing page

--- src/components/Scroller/Scroller.js
+++ src/components/Scroller/Scroller.js
@@ -5,12 +5,114 @@ import "./Scroller.css";
 const Scroller = () => {
   return (
     <div className="Scroller">
-      <Link className="dot" activeClass="active" to="landing-page" spy={true} smooth={true} />
-      <Link className="dot" activeClass="active" to="biodata" spy={true} smooth={true} />
-      <Link className="dot" activeClass="active" to="portfolio-1" spy={true} smooth={true} />
-      <Link className="dot" activeClass="active" to="portfolio-2" spy={true} smooth={true} />
-      <Link className="dot" activeClass="active" to="portfolio-3" spy={true} smooth={true} />
-      <Link className="dot" activeClass="active" to="contact" spy={true} smooth={true} />
+      <Link
+        className="dot"
+        activeClass="active"
+        to="landing-page"
+        spy={true}
+        smooth={true}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          class="bi bi-house-door"
+          viewBox="0 0 16 16"
+        >
+          <path d="M8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4.5a.5.5 0 0 0 .5-.5v-4h2v4a.5.5 0 0 0 .5.5H14a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146zM2.5 14V7.707l5.5-5.5 5.5 5.5V14H10v-4a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v4H2.5z" />
+        </svg>
+      </Link>
+      <Link
+        className="dot"
+        activeClass="active"
+        to="biodata"
+        spy={true}
+        smooth={true}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          class="bi bi-person"
+          viewBox="0 0 16 16"
+        >
+          <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z" />
+        </svg>
+      </Link>
+      <Link
+        className="dot"
+        activeClass="active"
+        to="portfolio-1"
+        spy={true}
+        smooth={true}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          class="bi bi-files"
+          viewBox="0 0 16 16"
+        >
+          <path d="M13 0H6a2 2 0 0 0-2 2 2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h7a2 2 0 0 0 2-2 2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm0 13V4a2 2 0 0 0-2-2H5a1 1 0 0 1 1-1h7a1 1 0 0 1 1 1v10a1 1 0 0 1-1 1zM3 4a1 1 0 0 1 1-1h7a1 1 0 0 1 1 1v10a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V4z" />
+        </svg>
+      </Link>
+      <Link
+        className="dot"
+        activeClass="active"
+        to="portfolio-2"
+        spy={true}
+        smooth={true}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          class="bi bi-files"
+          viewBox="0 0 16 16"
+        >
+          <path d="M13 0H6a2 2 0 0 0-2 2 2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h7a2 2 0 0 0 2-2 2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm0 13V4a2 2 0 0 0-2-2H5a1 1 0 0 1 1-1h7a1 1 0 0 1 1 1v10a1 1 0 0 1-1 1zM3 4a1 1 0 0 1 1-1h7a1 1 0 0 1 1 1v10a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V4z" />
+        </svg>
+      </Link>
+      <Link
+        className="dot"
+        activeClass="active"
+        to="portfolio-3"
+        spy={true}
+        smooth={true}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          class="bi bi-files"
+          viewBox="0 0 16 16"
+        >
+          <path d="M13 0H6a2 2 0 0 0-2 2 2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h7a2 2 0 0 0 2-2 2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm0 13V4a2 2 0 0 0-2-2H5a1 1 0 0 1 1-1h7a1 1 0 0 1 1 1v10a1 1 0 0 1-1 1zM3 4a1 1 0 0 1 1-1h7a1 1 0 0 1 1 1v10a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V4z" />
+        </svg>
+      </Link>
+      <Link
+        className="dot"
+        activeClass="active"
+        to="contact"
+        spy={true}
+        smooth={true}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          class="bi bi-search"
+          viewBox="0 0 16 16"
+        >
+          <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+        </svg>
+      </Link>
     </div>
   );
 };
--- src/pages/1-LandingPage/LandingPage.js
+++ src/pages/1-LandingPage/LandingPage.js
@@ -1,7 +1,23 @@
 import "./LandingPage.css";
+import TypeAnimation from 'react-type-animation';
 
 const LandingPage = () => {
-  return <div className="LandingPage" id="landing-page"></div>;
+  return (
+    <div className="LandingPage" id="landing-page">
+      <div className="introduction">
+        <TypeAnimation
+          cursor={true}
+          sequence={[
+            "Hi, ",
+            2000,
+            "I’m a student at Bandung Institute of Technology and a future Software Engineer.",
+          ]}
+          wrapper="a"
+          repeat={1}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default LandingPage;
